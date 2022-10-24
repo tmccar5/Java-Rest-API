d8a010a166265087cfc4cd1db59a915c21268504
Thomas McCarthy <tommccarthyguitar@gmail.com>
2022-10-23 19:15:57 -0500

    rest of controller completed

--- src/main/java/tom/streamdemo/controller/LiveStreamController.java
+++ src/main/java/tom/streamdemo/controller/LiveStreamController.java
@@ -1,9 +1,9 @@
 package tom.streamdemo.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import tom.streamdemo.exception.LiveStreamNotFoundException;
 import tom.streamdemo.model.LiveStream;
 import tom.streamdemo.repository.LiveStreamRepository;
 
@@ -24,4 +24,26 @@ public class LiveStreamController {
         return repository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public LiveStream findById(@PathVariable String id) throws LiveStreamNotFoundException {
+        return repository.findById(id);
+    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public LiveStream create(@RequestBody LiveStream stream) {
+        return repository.create(stream);
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping
+    public LiveStream update(@RequestBody LiveStream stream, @PathVariable String id) {
+        repository.update(stream, id);
+        return null;
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public LiveStream delete(@PathVariable String id) {
+        repository.delete(id);
+        return null;
+    }
 }
